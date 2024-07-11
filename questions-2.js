01bc79d3c2f03a08a46b13c69bd489bd828ed6a9
profrodrigociencias <rod-dias-gold@hotmail.com>
2024-07-10 21:39:06 -0300

    commit inicial
    primeiro arquivos carregados e públicos

---
+++ questions-2.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "3-Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language 3-Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Languagessss",
+    options: [
+      "1-Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text PreprocessorHyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor Hyper Text Preprocessor",
+      "2-Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup LanguageHyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language Hyper Text Markup Language",
+      "3-Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language 3-Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Language Hyper Text Multiple Languagessss",
+      "4-Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "4-Cascading Style Sheet",
+    options: [
+      "1-Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet Common Style Sheet",
+      "2-Colorful Style Sheet",
+      "3-Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet Computer Style Sheet",
+      "4-Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "1-Hypertext Preprocessor",
+    options: [
+      "1-Hypertext Preprocessor",
+      "2-Hypertext Programming",
+      "3-Hypertext Preprogramming",
+      "4-Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "4-Structured Query Language",
+    options: [
+      "1-Stylish Question Language",
+      "2-Stylesheet Query Language",
+      "3-Statement Question Language",
+      "4-Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "1-eXtensible Markup Language",
+    options: [
+      "1-eXtensible Markup Language",
+      "2-eXecutable Multiple Language",
+      "3-eXTra Multi-Program Language",
+      "4-eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
